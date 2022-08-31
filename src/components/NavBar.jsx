946099ca5e6c83697be7bff5d05c480cc3761703
Badrudeen Oluwafemi <phlemb6362@gmail.com>
2022-08-31 07:04:24 +0100

    responsiveness on different mobile phones done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,15 +10,13 @@ const NavBar = ({ handleChange }) => {
 
     return (
         <div className="navbar">
-            <Link to="/"><h3>Trade<span style={{color:'#73BEC3'}}>Watch</span></h3></Link>
+            <Link to="/"><h4>Trade<span style={{color:'#73BEC3'}}>Watch</span></h4></Link>
+               
+            <FiRefreshCcw onClick={ handleClick } className="refresh-icon" />
 
-            <div className="right">
-                <FiRefreshCcw onClick={ handleClick } className="refresh-icon" />
-    
-                <div className="search-box">
-                    <BiSearchAlt className="search-icon" />
-                    <input onChange={(e) => handleChange(e.target.value)} placeholder="Search..." />
-                </div>
+            <div className="search-box">
+                <BiSearchAlt className="search-icon" />
+                <input onChange={(e) => handleChange(e.target.value)}placeholder="Search..." />
             </div>
         </div>
 
